84caba02d25d71e7719724223c4887c91fa5a2f8
Nikarus2370 <32275242+Nikarus2370@users.noreply.github.com>
2018-02-26 16:08:52 -0500

    C

---
+++ Lab 7A -Simple Array Class C#/Simple Array Class/Simple Array Class/MyArrayClass.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Array_Class
+{
+    class MyArrayClass
+    {
+        //Two fields or instance variables. 
+        private int[] arr;
+        private int iNumElements;   //# of elements in the array, not the size of the array.
+
+        //Constructor. Initializes both instance variables. Does not
+        // create a new array.
+        public MyArrayClass(int[] array, int iNumElementsLoc)
+        {
+            arr = array;
+            iNumElements = iNumElementsLoc;
+        }
+
+        // Displays the ith element
+        public void DisplayAt(int i)
+        {
+            Console.Write($"Value at index {i} is: {arr[i]}\n");
+        }
+
+        // Display all elements in the array that have a value
+        public void DisplayAll()
+        {
+            for (int i = 0; i < iNumElements; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        // Key - value to search in the array
+        // return value - index where key was found
+        public int Find(int key)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (key == arr[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        // insert an element in the array.
+        public void Insert(int n)
+        {
+            arr[iNumElements] = n;
+            iNumElements++;
+        }
+
+
+        // delete 'key' from the array. 
+        // which means that you must move up rest of the elements.
+        public void Delete(int key)
+        {
+            int loc = Find(key);
+            if (loc > -1)
+            {
+                for (int i = loc; i < iNumElements; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                iNumElements--;
+            }
+        }
+
+
+        // sort the array using bubble sort
+        public void BubbleSort()
+        {
+            int outInt, inInt;
+            int temp;
+
+            for (outInt = iNumElements - 1; outInt > 0; outInt--) // outer loop (backward)
+		{
+                for (inInt = 0; inInt < outInt; inInt++) // inner loop (forward)
+			{
+
+                    // combined the old "swap" into the "if" loop
+                    if (arr[inInt] > arr[inInt + 1])
+                    {
+                        temp = arr[inInt];
+                        arr[inInt] = arr[inInt + 1];
+                        arr[inInt + 1] = temp;
+                    } // end if
+                } // inner for loop
+            } // outer for loop
+        } // end bubbleSort()
+    }
+}
---
+++ Lab 7A -Simple Array Class C#/Simple Array Class/Simple Array Class/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Array_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            int[] arr = new int[100];
+            arr[0] = 77;            // insert 10 items
+            arr[1] = 99;
+            arr[2] = 44;
+            arr[3] = 55;
+            arr[4] = 22;
+            arr[5] = 88;
+            arr[6] = 11;
+            arr[7] = 3;
+            arr[8] = 66;
+            arr[9] = 33;
+            int nElems = 10;            // now 10 items in array
+
+            MyArrayClass arrayObj = new MyArrayClass(arr, nElems);
+            Console.Write("\nDisplaying all members of the array\n");
+            arrayObj.DisplayAll();
+
+            Console.Write("\nDisplaying element at index 6 of the array\n");
+            arrayObj.DisplayAt(6);
+
+            Console.Write("\nFind 55 in the array\n");
+            if (arrayObj.Find(55) != -1)
+            {
+                Console.Write("Number 55 was found at index %d of the array\n", arrayObj.Find(55));
+            }
+            else
+            {
+                Console.Write("Number 55 was NOT found.\n");
+            }
+
+
+            Console.Write("\nDelete 55 from the array\n");
+            arrayObj.Delete(55);
+
+            Console.Write("\nFind 55 in the array\n");
+            if (arrayObj.Find(55) != -1)
+            {
+                Console.Write("Number 55 was found at index %d of the array\n", arrayObj.Find(55));
+            }
+            else
+            {
+                Console.Write("Number 55 was NOT found.\n");
+            }
+
+            Console.Write("\nDisplaying all members of the array\n");
+            arrayObj.DisplayAll();
+
+            Console.Write("\nSorting the array\n");
+            arrayObj.BubbleSort();
+
+            Console.Write("\nDisplaying all members of the array\n");
+            arrayObj.DisplayAll();
+
+            Console.Write("\nInsert 49 into the array and then display array\n");
+            arrayObj.Insert(49);
+
+            arrayObj.DisplayAll();
+        }
+    }
+}
